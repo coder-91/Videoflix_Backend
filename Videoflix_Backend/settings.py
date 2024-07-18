a5692f39b52f645f2f4ebc36003c79e240835444
VeyselKaraali <veysel-karaali@outlook.de>
2024-07-18 16:14:22 +0200

    - pip install django-redis - Redis-config

--- Videoflix_Backend/settings.py
+++ Videoflix_Backend/settings.py
@@ -126,5 +126,16 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "videoflix"
+    }
+}
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
