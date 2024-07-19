67c9f5e3576a34e28de90bccf8619b223e79ebd9
VeyselKaraali <veysel-karaali@outlook.de>
2024-07-19 16:23:01 +0200

    Custom Admin Forms

--- user/admin.py
+++ user/admin.py
@@ -3,4 +3,26 @@ from django.contrib import admin
 from user.models import CustomUser
 
 # Register your models here.
-admin.site.register(CustomUser)
+from django.contrib import admin
+from .models import CustomUser
+from .forms import CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
+
+
+# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    add_form = CustomUserCreationForm
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'Individual Data',
+            {
+                'fields': (
+                    'custom',
+                    'phone',
+                    'address'
+                )
+            }
+        )
+    )
